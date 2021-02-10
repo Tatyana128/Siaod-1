eba97b5f8bf7d87bbe187f8c136b25652f4b2c5c
Tatyana128 <74492816+Tatyana128@users.noreply.github.com>
2021-02-11 00:50:33 +0300

    Add files via upload

---
+++ Лабораторная работа №1 (Сортировки)/Лабораторная работа №1 (Сортировки)/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Лабораторная_работа__1__Сортировки_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.SortClassic = new System.Windows.Forms.RadioButton();
+            this.SortFast = new System.Windows.Forms.RadioButton();
+            this.SortShella = new System.Windows.Forms.RadioButton();
+            this.SortExchange = new System.Windows.Forms.RadioButton();
+            this.SortInsert = new System.Windows.Forms.RadioButton();
+            this.SortChoice = new System.Windows.Forms.RadioButton();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.GroupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.SortClassic);
+            this.GroupBox1.Controls.Add(this.SortFast);
+            this.GroupBox1.Controls.Add(this.SortShella);
+            this.GroupBox1.Controls.Add(this.SortExchange);
+            this.GroupBox1.Controls.Add(this.SortInsert);
+            this.GroupBox1.Controls.Add(this.SortChoice);
+            this.GroupBox1.Font = new System.Drawing.Font("Bookman Old Style", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GroupBox1.Location = new System.Drawing.Point(12, 22);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(926, 271);
+            this.GroupBox1.TabIndex = 0;
+            this.GroupBox1.TabStop = false;
+            this.GroupBox1.Text = "Выберите вид сортировки:";
+            // 
+            // SortClassic
+            // 
+            this.SortClassic.AutoSize = true;
+            this.SortClassic.Location = new System.Drawing.Point(436, 107);
+            this.SortClassic.Name = "SortClassic";
+            this.SortClassic.Size = new System.Drawing.Size(438, 43);
+            this.SortClassic.TabIndex = 7;
+            this.SortClassic.TabStop = true;
+            this.SortClassic.Text = "встроенная сортировка ";
+            this.SortClassic.UseVisualStyleBackColor = true;
+            this.SortClassic.CheckedChanged += new System.EventHandler(this.SortClassic_CheckedChanged);
+            // 
+            // SortFast
+            // 
+            this.SortFast.AutoSize = true;
+            this.SortFast.Location = new System.Drawing.Point(436, 58);
+            this.SortFast.Name = "SortFast";
+            this.SortFast.Size = new System.Drawing.Size(387, 43);
+            this.SortFast.TabIndex = 6;
+            this.SortFast.TabStop = true;
+            this.SortFast.Text = "быстрая сортировка ";
+            this.SortFast.UseVisualStyleBackColor = true;
+            this.SortFast.CheckedChanged += new System.EventHandler(this.SortFast_CheckedChanged);
+            // 
+            // SortShella
+            // 
+            this.SortShella.AutoSize = true;
+            this.SortShella.Location = new System.Drawing.Point(17, 205);
+            this.SortShella.Name = "SortShella";
+            this.SortShella.Size = new System.Drawing.Size(342, 43);
+            this.SortShella.TabIndex = 3;
+            this.SortShella.TabStop = true;
+            this.SortShella.Text = "сортировка Шелла";
+            this.SortShella.UseVisualStyleBackColor = true;
+            this.SortShella.CheckedChanged += new System.EventHandler(this.SortShella_CheckedChanged);
+            // 
+            // SortExchange
+            // 
+            this.SortExchange.AutoSize = true;
+            this.SortExchange.Location = new System.Drawing.Point(17, 156);
+            this.SortExchange.Name = "SortExchange";
+            this.SortExchange.Size = new System.Drawing.Size(380, 43);
+            this.SortExchange.TabIndex = 2;
+            this.SortExchange.TabStop = true;
+            this.SortExchange.Text = "сортировка обменом";
+            this.SortExchange.UseVisualStyleBackColor = true;
+            this.SortExchange.CheckedChanged += new System.EventHandler(this.SortExchange_CheckedChanged);
+            // 
+            // SortInsert
+            // 
+            this.SortInsert.AutoSize = true;
+            this.SortInsert.Location = new System.Drawing.Point(17, 107);
+            this.SortInsert.Name = "SortInsert";
+            this.SortInsert.Size = new System.Drawing.Size(391, 43);
+            this.SortInsert.TabIndex = 1;
+            this.SortInsert.TabStop = true;
+            this.SortInsert.Text = "сортировка вставкой";
+            this.SortInsert.UseVisualStyleBackColor = true;
+            this.SortInsert.CheckedChanged += new System.EventHandler(this.SortInsert_CheckedChanged);
+            // 
+            // SortChoice
+            // 
+            this.SortChoice.AutoSize = true;
+            this.SortChoice.Checked = true;
+            this.SortChoice.Location = new System.Drawing.Point(17, 58);
+            this.SortChoice.Name = "SortChoice";
+            this.SortChoice.Size = new System.Drawing.Size(394, 43);
+            this.SortChoice.TabIndex = 0;
+            this.SortChoice.TabStop = true;
+            this.SortChoice.Text = "сортировка выбором ";
+            this.SortChoice.UseVisualStyleBackColor = true;
+            this.SortChoice.CheckedChanged += new System.EventHandler(this.SortChoice_CheckedChanged);
+            // 
+            // btnGo
+            // 
+            this.btnGo.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.btnGo.Font = new System.Drawing.Font("Bookman Old Style", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnGo.Location = new System.Drawing.Point(264, 317);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(442, 78);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "Выполнить сортировку сгенерированной матрицы";
+            this.btnGo.UseVisualStyleBackColor = false;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(963, 407);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.GroupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Сортировки";
+            this.GroupBox1.ResumeLayout(false);
+            this.GroupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GroupBox1;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.RadioButton SortFast;
+        private System.Windows.Forms.RadioButton SortShella;
+        private System.Windows.Forms.RadioButton SortExchange;
+        private System.Windows.Forms.RadioButton SortInsert;
+        private System.Windows.Forms.RadioButton SortChoice;
+        private System.Windows.Forms.RadioButton SortClassic;
+    }
+}
+
---
+++ Лабораторная работа №1 (Сортировки)/Лабораторная работа №1 (Сортировки)/Form1.cs
@@ -0,0 +1,318 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Лабораторная_работа__1__Сортировки_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int MatrixSize = 1000;
+
+
+        #region Выбор сортировки 
+        int Sort = 1;
+        String SortString = "сортировка выбором";
+        private void SortChoice_CheckedChanged(object sender, EventArgs e)
+        {
+            Sort = 1;
+            SortString = "сортировка выбором";
+        }
+
+        private void SortInsert_CheckedChanged(object sender, EventArgs e)
+        {
+            Sort = 2;
+            SortString = "сортировка вставками";
+        }
+
+        private void SortExchange_CheckedChanged(object sender, EventArgs e)
+        {
+            Sort = 3;
+            SortString = "сортировка обменом";
+        }
+
+        private void SortShella_CheckedChanged(object sender, EventArgs e)
+        {
+            Sort = 4;
+            SortString = "сортировка Шелла";
+        }
+
+        private void SortFast_CheckedChanged(object sender, EventArgs e)
+        {
+            Sort = 5;
+            SortString = "сортировка быстрая";
+        }
+        private void SortClassic_CheckedChanged(object sender, EventArgs e)
+        {
+            Sort = 6;
+            SortString = "сортировка встроенная";
+        }
+
+        #endregion
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            int[,] FirstMatrix = CreateMatrix();
+
+
+            MessageBox.Show("Матрица сгенерирована, будет выполняться сортировка");
+
+            Stopwatch timer=null;
+
+            switch (Sort)
+            {
+                case 1:
+                    timer = Stopwatch.StartNew();
+                    ChoiceSort(FirstMatrix);
+                    timer.Stop();
+                    break;
+                case 2:
+                    timer = Stopwatch.StartNew();
+                    InsertsSort(FirstMatrix);
+                    timer.Stop();
+                    break;
+                case 3:
+                    timer = Stopwatch.StartNew();
+                    ExchangeSort(FirstMatrix);
+                    timer.Stop();
+                    break;
+                case 4:
+                    timer = Stopwatch.StartNew();
+                    ShellaSort(FirstMatrix);
+                    timer.Stop();
+                    break;
+                case 5:
+                    timer = Stopwatch.StartNew();
+                    for (int k = 0; k < MatrixSize; k++)
+                    {
+                        QuickSort(FirstMatrix, 0, MatrixSize- 1, k);
+                    }                     
+                    timer.Stop();
+                    break;
+                case 6:
+                    timer = Stopwatch.StartNew();
+                    for (int i = 0; i < MatrixSize; i++)
+                    {
+                        timer.Start();
+                        int[] MyArray = new int[MatrixSize];
+                        for (int j = 0; j < MatrixSize; j++)
+                        {
+                            MyArray[j]=FirstMatrix[i, j];
+                        }                   
+                        Array.Sort(MyArray);
+                        timer.Stop();
+                        for (int j = 0; j < MatrixSize; j++)
+                        {
+                            FirstMatrix[i,j]=MyArray[j];
+                        }
+                    }                 
+                    break;
+            }
+            
+            MessageBox.Show("Сортировка закончена, выполните сохранение");
+
+            SaveMatrix(FirstMatrix, SortString, timer.ElapsedMilliseconds.ToString());
+        }
+
+        #region Генерация матрицы
+        int[,] CreateMatrix()
+        {
+            int[,] Matrix = new int[MatrixSize, MatrixSize];
+            Random rand = new Random();
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                for (int j = 0; j < MatrixSize; j++)
+                {
+                    Matrix[i, j] = rand.Next(0, 5001);
+                }
+            }
+            return Matrix;
+        }
+        #endregion
+        #region Сохранение
+
+
+
+        public void SaveMatrix(int[,] Matrix2, String str, String timeSort)
+        {
+            Microsoft.Office.Interop.Excel.Application excelapp = new Microsoft.Office.Interop.Excel.Application();
+            excelapp.Visible = true;
+
+            _Workbook workbook = excelapp.Workbooks.Add(Type.Missing);
+            _Worksheet worksheet = (_Worksheet)workbook.ActiveSheet;
+
+            excelapp.UserControl = false;
+
+
+            bool failed = false;
+            do
+            {
+                try
+                {
+                    worksheet.Cells[1, 1] = "Отсортированнная  матрица. " + "Была применена " + str + ". Время выполнения сортировки = " + timeSort + " мс";
+                    for (int i = 0; i < MatrixSize; i++)
+                    {
+                        for (int j = 0; j < MatrixSize; j++)
+                        {
+                            worksheet.Cells[i + 2, j + 1] = Matrix2[i, j];
+                        }
+                    }
+
+                    workbook.Save();
+
+                    failed = false;
+                }
+                catch (System.Runtime.InteropServices.COMException e)
+                {
+                    failed = true;
+                }
+                System.Threading.Thread.Sleep(10);
+
+            } while (failed);
+        }
+        #endregion
+        #region Cортировки
+        void ChoiceSort(int[,] ListForSort)
+        {
+            for (int k = 0; k < MatrixSize; k++)
+            {
+                int index = 0;
+
+                for (int i = 0; i < MatrixSize - 1; i++)
+                {
+                    index = i;
+
+                    for (int j = i + 1; j < MatrixSize; j++)
+                    {
+                        if (ListForSort[k, j].CompareTo(ListForSort[k, index]) == -1)
+                        {
+                            index = j;
+                        }
+                    }
+
+                    if (index != i)
+                    {
+                        int temp = ListForSort[k, i];
+                        ListForSort[k, i] = ListForSort[k, index];
+                        ListForSort[k, index] = temp;
+                    }
+                }
+            }
+        }
+
+
+        void InsertsSort(int[,] ListForSort)
+        {
+
+            for (int k = 0; k < MatrixSize; k++)
+            {
+                for (int i = 0; i < MatrixSize; i++)
+                {
+                    var temp = ListForSort[k, i];
+                    var j = i;
+                    while (j > 0 && temp.CompareTo(ListForSort[k, j - 1]) == -1)
+                    {
+                        ListForSort[k, j] = ListForSort[k, j - 1];
+                        j--;
+                    }
+                    ListForSort[k, j] = temp;
+                }
+
+            }
+        }
+
+        void ExchangeSort(int[,] ListForSort)
+        {
+
+            for (int k = 0; k < MatrixSize; k++)
+            {
+                int count = MatrixSize;
+                for (int j = 0; j < count; j++)
+                {
+                    for (int i = 0; i < count - 1 - j; i++)
+                    {
+                        var a = ListForSort[k, i];
+                        var b = ListForSort[k, i + 1];
+                        if (a.CompareTo(b) == 1)
+                        {
+                            int temp = ListForSort[k, i];
+                            ListForSort[k, i] = ListForSort[k, i + 1];
+                            ListForSort[k, i + 1] = temp;
+                        }
+
+                    }
+                    count--;
+                }
+
+            }
+        }
+
+        void ShellaSort(int[,] ListForSort)
+        {
+
+            for (int k = 0; k < MatrixSize; k++)
+            {
+                int step = MatrixSize / 2;
+
+                while (step > 0)
+                {
+                    for (int i = step; i < MatrixSize; i++)
+                    {
+                        int j = i;
+                        while (j >= step && ListForSort[k, j - step].CompareTo(ListForSort[k, j]) == 1)
+                        {
+                            int temp = ListForSort[k, j - step];
+                            ListForSort[k, j - step] = ListForSort[k, j];
+                            ListForSort[k, j] = temp;
+                            j -= step;
+                        }
+                    }
+                    step /= 2;
+                }
+            }
+        }
+
+        void QuickSort(int[,] ListForSort, int left, int right, int k)
+        {
+            if (left >= right) { return; }
+            else
+            {
+                var pivot = Sorting(ListForSort, left, right, k);
+                QuickSort(ListForSort, left, pivot - 1, k);
+                QuickSort(ListForSort, pivot + 1, right, k);
+            }
+        }
+
+        int Sorting(int[,] ListForSort, int left, int right, int k)
+        {
+            var pointer = left;
+            for (int i = left; i <= right; i++)
+            {
+                if (ListForSort[k, i].CompareTo(ListForSort[k, right]) == -1)
+                {
+                    int temp1 = ListForSort[k, i];
+                    ListForSort[k, i] = ListForSort[k, pointer];
+                    ListForSort[k, pointer] = temp1;
+                    pointer++;
+                }
+            }
+
+            int temp = ListForSort[k, right];
+            ListForSort[k, right] = ListForSort[k, pointer];
+            ListForSort[k, pointer] = temp;
+
+            return pointer;
+        }
+
+
+        #endregion
+
+
+    }
+}
